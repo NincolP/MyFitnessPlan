1069870ca6ae7f26952a2b4b279444a707a37539
Nincol <perezn2@uhv.edu>
2024-11-11 17:07:27 -0600

    removing extra lines in the code

--- src/main/java/org/example/demo1/HelloApplication.java
+++ src/main/java/org/example/demo1/HelloApplication.java
@@ -10,18 +10,11 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-
-        //System.out.println("javafx.runtime.version: " + System.getProperty("javafx.runtime.version"));
-
-        //System.exit(0);
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 520, 440);
         stage.setTitle("Your Fitness Plan!");
         stage.setScene(scene);
         stage.show();
-
-
-
 
     }
 
